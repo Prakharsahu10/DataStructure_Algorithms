37a2b585bc990eb21418f649e1b9dadf71810b65
Prakharsahu10 <prakhars762@gmail.com>
2024-11-26 19:28:36 +0530

    initial commit

---
+++ STACKS AND QUEUES/Next_Greater_Element_1.cpp
@@ -0,0 +1,38 @@
+/* Next Greater Element 1 using Stack */
+
+#include <vector>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+    {
+        unordered_map<int, int> hm;
+        stack<int> st;
+
+        // Fill the hashmap with next greater elements for nums2
+        for (int i = nums2.size() - 1; i >= 0; i--)
+        {
+            while (!st.empty() && st.top() < nums2[i])
+            {
+                st.pop();
+            }
+            if (st.empty())
+                hm[nums2[i]] = -1;
+            else
+                hm[nums2[i]] = st.top();
+            st.push(nums2[i]);
+        }
+
+        vector<int> ans(nums1.size());
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            ans[i] = hm[nums1[i]];
+        }
+
+        return ans;
+    }
+};
