a4136f09e9a357a35ef2e7c310a80471e4172b1c
Prakhar Sahu <prakhars762@gmail.com>
2025-03-12 23:52:29 +0530

    initial commit

--- STRINGS/Reverse_words_in_string.cpp
+++ STRINGS/Reverse_words_in_string.cpp
@@ -41,27 +41,27 @@ class Solution
 public:
     string reverseWords(string s)
     {
+        int n = s.length();
+        string ans = "";
+
         reverse(s.begin(), s.end());
-        int n = s.size();
-        int left = 0;
-        int right = 0;
-        int i = 0;
-        while (i < n)
+
+        for (int i = 0; i < n; i++)
         {
-            while (i < n && s[i] == ' ')
-                i++;
-            if (i == n)
-                break;
+            string word = "";
             while (i < n && s[i] != ' ')
             {
-                s[right++] = s[i++];
+                word += s[i];
+                i++;
             }
-            reverse(s.begin() + left, s.begin() + right);
-            s[right++] = ' ';
-            left = right;
-            i++;
+
+            reverse(word.begin(), word.end());
+            if (word.length() > 0)
+            {
+                ans += " " + word;
+            }
         }
-        s.resize(right - 1);
-        return s;
+
+        return ans.substr(1);
     }
 };
